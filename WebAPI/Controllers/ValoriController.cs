b6e35d73ae4c071798d0f838413797de69950196
Cristian_Badea <badea_cristian_alexandru@yahoo.com>
2015-04-29 17:26:36 +0300

    29 aprilie primul comit

---
+++ WebAPI/Controllers/ValoriController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+    public class ValoriController : ApiController
+    {
+        static List<string> valori = new List<string>() { "valoare 1", "valoare 2" };
+
+
+
+
+        // GET: api/Valori
+        public IEnumerable<string> Get()
+        {
+            return valori;
+        }
+
+        // GET: api/Valori/5
+        public string Get(int id)
+        {
+            if (id > valori.Count - 1)
+            {
+                var raspuns = new HttpResponseMessage();
+                raspuns.StatusCode = HttpStatusCode.NotFound;
+                raspuns.Content = new StringContent("Elementul nu exista in lista");
+                throw new HttpResponseException(raspuns);
+            }
+            
+                return valori[id];
+            
+        }
+
+        // POST: api/Valori
+        public void Post([FromBody]string value)
+        {
+        
+            valori.Add(value);
+        }
+
+        // PUT: api/Valori/5
+        public void Put(int id, [FromBody]string value)
+        {
+            valori[id] = value;
+        }
+
+        // DELETE: api/Valori/5
+        public void Delete(int id)
+        {
+            valori.RemoveAt(id);
+        }
+    }
+}
---
+++ WebAPI/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string adresa = "http://localhost:8088";
+
+            using (WebApp.Start<Startup>(adresa))
+            {
+                Console.WriteLine("Apasa o tasta pt inchiderea serviciului");
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
